32e62cd751af2e6201bb7892f12fe3b27e6d0b00
coderfee <coderfee@outlook.com>
2019-04-20 11:26:55 +0800

    :construction: Add new feature
    Fetch do/wish/collect

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const fs = require('fs');
 const puppeteer = require('puppeteer-core');
+const chalk = require('chalk');
+
 const config = require('./config');
+const log = console.log;
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
-    executablePath:
-      'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+    // headless: false,
+    executablePath: '/opt/google/chrome/google-chrome'
   });
   const page = await browser.newPage();
+  log(chalk.yellow('服务已启动...'));
   await page.goto(
     'https://accounts.douban.com/passport/login_popup?login_source=anony'
   );
@@ -17,28 +20,59 @@ const config = require('./config');
   await page.type('#password', config.password);
   await page.click('.account-form-field-submit');
   await page.waitForNavigation({ waitUntil: 'domcontentloaded' });
+  log(chalk.green(`登录成功：${config.username}`));
+
+  await page.goto('https://book.douban.com/people/84902716/wish');
+  const wish = await resolveBooks('wish');
+  await page.waitFor(2000);
+
   await page.goto('https://book.douban.com/people/84902716/do');
+  const dos = await resolveBooks('do');
+  await page.waitFor(2000);
 
-  const items = await page.$$eval('.subject-item', els => {
-    return els.map(item => {
-      return {
-        status: 'do',
-        img: item.getElementsByTagName('img')[0].src,
-        title: item
-          .getElementsByTagName('h2')[0]
-          .textContent.replace(/\s/g, ''),
-        author: item
-          .getElementsByClassName('pub')[0]
-          .textContent.split('/')[0]
-          .trim(),
-        comment: item.getElementsByClassName('comment')[0].textContent.trim()
-      };
+  await page.goto('https://book.douban.com/people/84902716/collect');
+  const collect = await resolveBooks('collect');
+
+  fs.writeFile(
+    'books.json',
+    JSON.stringify([...wish, ...dos, ...collect]),
+    async err => {
+      if (err) console.log(err);
+      log(
+        chalk.green(
+          `想读： ${wish.length}  在读：${dos.length}  读过：${collect.length}`
+        )
+      );
+      await browser.close();
+    }
+  );
+
+  async function resolveBooks(status) {
+    const books = await page.$$eval('.subject-item', (els, status) => {
+      return els.map(item => {
+        return {
+          status,
+          img: item.getElementsByTagName('img')[0].src,
+          title: item
+            .getElementsByTagName('h2')[0]
+            .textContent.replace(/\s/g, ''),
+          author: item
+            .getElementsByClassName('pub')[0]
+            .textContent.split('/')[0]
+            .trim(),
+          comment: item.getElementsByClassName('comment')[0].textContent.trim()
+        };
+      });
+    }, status);
+    const nextBtn = await page.evaluate(() => {
+      const el = document.querySelector('.paginator .next a');
+      if (el) return true;
+      return false;
     });
-  });
-
-  fs.writeFile('books.json', JSON.stringify(items), err => {
-    if (err) process.exit(0);
-    console.log('done');
-  });
-  await browser.close();
+    if (!nextBtn) return books;
+    await page.click('.paginator .next a');
+    await page.waitForSelector('.subject-item');
+    const data = await resolveBooks(status);
+    return books.concat(data);
+  }
 })();
